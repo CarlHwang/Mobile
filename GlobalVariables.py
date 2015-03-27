f7fb8758219fcf743da05a6b142050cd1f96e1af
Carl <Carl@Carl-MPB.local>
2015-03-27 21:40:09 +0800

    add GlobalVariables.py File

---
+++ GlobalVariables.py
@@ -0,0 +1,11 @@
+
+# item set denotes the item and category table
+
+# distinct item amount (in item set)
+item_amount = 314695
+
+# distinct geohash amount (in item set)
+gh_amount = 45290
+
+# distinct category amount (in item set)
+cat_amount = 954
