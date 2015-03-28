bbb7127c59be975a0ccb355a3a8450a4b9f5e0b3
Carl <Carl@Carl-MPB.local>
2015-03-28 14:23:33 +0800

    add comments

--- DataCleaning.py
+++ DataCleaning.py
@@ -2,10 +2,13 @@
 # -*- coding:utf-8 -*-
 import csv
 
+# 把user列表里的时间字符串转换成整数表示
 def timeToInt():
+    # 输出的文件头
     outfile = open('train_user_time_to_int.csv', 'wb')
     spamwriter = csv.writer(outfile, dialect = 'excel')
     spamwriter.writerow(['user_id', 'item_id', 'behavior_type', 'user_geohash', 'item_category', 'time'])
+    
     with open('train_user.csv', 'rb') as infile:
         reader = csv.reader(infile)
         for row in reader:
@@ -22,12 +25,15 @@ def timeToInt():
             day = int(time[8:10])
             hour = int(time[11:])
             num = 1
-
+            
+            # 计算相对时间
             if(month == 11):
                 num += (day-18)*24 + hour
             else:
                 num += 312 + (day-1)*24 + hour
+                
             spamwriter.writerow([user_id, item_id, behavior_type, user_geohash, item_category, num])
+            
             print month, day, hour, num
              
 if __name__ == '__main__':
