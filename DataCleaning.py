d55100fb630a084121f0eb2641aee550180d3be6
Carl <Carl@Carl-MPB.local>
2015-03-29 16:42:52 +0800

    update DecayCurve.py, update DataCleaning.py

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 import csv
+from _ast import Num
 
 # 把user列表里的时间字符串转换成整数表示
 def timeToInt():
@@ -35,7 +36,59 @@ def timeToInt():
             spamwriter.writerow([user_id, item_id, behavior_type, user_geohash, item_category, num])
             
             print month, day, hour, num
+            
+def cleanInvalidRecord():
+    # 输出的文件头
+    outfile = open('train_user_time_to_int_cleaned.csv', 'wb')
+    spamwriter = csv.writer(outfile, dialect = 'excel')
+    spamwriter.writerow(['user_id', 'item_id', 'behavior_type', 'user_geohash', 'item_category', 'time'])
+    
+    table = {}
+    with open('./FeatureExtraction/last_access_time.csv', 'rb') as infile:
+        reader = csv.reader(infile)
+        for row in reader:
+            user_id = row[0]
+            item_id = row[1]
+            click_gap1 = row[2]
+            click_gap2 = row[3]
+            collect_gap = row[4]
+            cart_gap = row[5]
+            time = row[6]
+            
+            if time == 'time':
+                continue
+            time = int(time)
+            
+            if click_gap1 == '-1' and click_gap2 == '-1' and collect_gap == '-1' and cart_gap == '-1':
+                key = user_id + ' ' + item_id
+                if not table.get(key):
+                    table[key] = time
+                else:
+                    table[key] = max(table[key], time)
+    
+    with open('train_user_time_to_int.csv', 'rb') as infile:
+        reader = csv.reader(infile)
+        num = 1
+        for row in reader:
+            user_id = row[0]
+            item_id = row[1]
+            time = row[5]
+            
+            if time == 'time':
+                continue
+            time = int(time)
+            
+            key = user_id + ' ' + item_id
+            if (not table.get(user_id + ' ' + item_id)) or table[key] < time:
+                
+                spamwriter.writerow(row)
+            else:
+                print num
+                num += 1
+                    
+            
+            
              
 if __name__ == '__main__':
-    timeToInt()
-    #test
+#     timeToInt()
+    cleanInvalidRecord()
--- FeatureExtraction/DecayCurve.py
+++ FeatureExtraction/DecayCurve.py
@@ -2,87 +2,167 @@
 # -*- coding:utf-8 -*-
 
 import csv
-from numpy.f2py.auxfuncs import throw_error
 
-clickTable = {}
-collectTable = {}
-cartTable = {}
+
 dealTable = {}
-
-lastAccessTimeTable = []
 
 def lastAccessTime():
     pass
 
-def lastAccessTimeOverItem():
+def initTableOverItem():   
     # 输出的文件头
     outfile = open('last_access_time.csv', 'wb')
     spamwriter = csv.writer(outfile, dialect = 'excel')
-    spamwriter.writerow(['user_id', 'item_id', 'last_access_gap'])
+    spamwriter.writerow(['user_id', 'item_id', 'click_gap1', 'click_gap2','collect_gap','cart_gap','time'])
     
-
+    outfile_click1 = open('train_user_time_to_int_click1.csv', 'wb')
+    spamwriter_click1 = csv.writer(outfile_click1, dialect = 'excel')
+    spamwriter_click1.writerow(['user_id', 'item_id','time'])
+    
+    outfile_click2 = open('train_user_time_to_int_click2.csv', 'wb')
+    spamwriter_click2 = csv.writer(outfile_click2, dialect = 'excel')
+    spamwriter_click2.writerow(['user_id', 'item_id','time'])
+    
+    outfile_collect = open('train_user_time_to_int_collect.csv', 'wb')
+    spamwriter_collect = csv.writer(outfile_collect, dialect = 'excel')
+    spamwriter_collect.writerow(['user_id', 'item_id','time'])
+    
+    outfile_cart = open('train_user_time_to_int_cart.csv', 'wb')
+    spamwriter_cart = csv.writer(outfile_cart, dialect = 'excel')
+    spamwriter_cart.writerow(['user_id', 'item_id','time'])
+    
     with open('../train_user_time_to_int.csv', 'rb') as infile:
         reader = csv.reader(infile)
+        num = 1
         for row in reader:
             user_id = row[0]
             item_id = row[1]
             behavior_type = row[2]
             time = row[5]
             
-            if(len(time) > 3):
+            if time == 'time':
                 continue
-            
-            time = int(time)
-            if behavior_type == '1':
-                addToTable(clickTable, user_id +' '+ item_id, time)
+            if behavior_type == '4':
+                spamwriter.writerow([user_id, item_id, -1, -1, -1, -1, time])
+                print num, time
+                
+            elif behavior_type == '1':
+                if num <= 5000000:
+                    spamwriter_click1.writerow([user_id, item_id, time])
+                else:
+                    spamwriter_click2.writerow([user_id, item_id, time])
+                num += 1
             elif behavior_type == '2':
-                addToTable(collectTable, user_id +' '+ item_id, time)
-            elif behavior_type == '3':
-                addToTable(cartTable, user_id +' '+ item_id, time)
+                spamwriter_collect.writerow([user_id, item_id, time])
             else:
-                addToTable(dealTable, user_id +' '+ item_id, time)
-        
-        
-    print "建表完成"
-        
-    for key in dealTable.keys():
-        times = dealTable[key]
-        for time in times:
-            lastClick = getLastAccessTime(clickTable, key, time)
-            lastCollect = getLastAccessTime(collectTable, key, time)
-            lastCart = getLastAccessTime(cartTable, key, time)
-            last = max(lastClick, lastCollect, lastCart)
+                spamwriter_cart.writerow([user_id, item_id, time])
+
             
-            gap = time - last
-            print "gap", gap, "购买时间：", time, "点击", lastClick, "收藏", lastCollect, "购物车", lastCart
-            if gap < 0:
-                raise Exception("gap小于0")
-            heads = key.split()
-            lastAccessTimeTable.append([heads[0], heads[1], gap])
+def lastAccessTimeOverItemForBeh(btype):
+    table = {}
+    
+    bfile = ''
+    if btype == '1.1':
+        bfile = 'train_user_time_to_int_click1.csv'
+    elif btype == '1.2':
+        bfile = 'train_user_time_to_int_click2.csv'
+    elif btype == '2':
+        bfile = 'train_user_time_to_int_collect.csv'
+    else:
+        bfile = 'train_user_time_to_int_cart.csv'
+        
+    with open(bfile, 'rb') as infile:
+        reader = csv.reader(infile)
+        num = 1
+        for row in reader:
+            user_id = row[0]
+            item_id = row[1]
+            time = row[2]
+             
+            if(time == 'time'):
+                continue
+             
+            time = int(time)
+            addToTable(table, user_id +' '+ item_id, time)
+            print num, btype, time
+            num+=1
+            
+    writeBehGap(btype, table)
+
+
+def writeBehGap(btype, table):
+    lastAccessTimeTable = []
+    with open('last_access_time.csv', 'rb') as infile:
+        reader = csv.reader(infile)
+        num = 1
+        for row in reader:
+            user_id = row[0]
+            item_id = row[1]
+            click_gap1 = row[2]
+            click_gap2 = row[3]
+            collect_gap = row[4]
+            cart_gap = row[5]
+            time = row[6]
+            
+            if time == 'time':
+                continue
+            time = int(time)
+            lastBeh = -1
+            if user_id == '112148363' and item_id == '390292615':
+                lastBeh = getLastAccessTime(table, user_id +' '+ item_id, time)
+            else:
+                lastBeh = getLastAccessTime(table, user_id +' '+ item_id, time)
+            gap = -1
+            if not lastBeh == -1:
+                gap = time - lastBeh
+            print num, "   gap:", gap, "购买时间：", time, "行为", lastBeh
+            num += 1
+            if btype == '1.1':
+                lastAccessTimeTable.append([user_id, item_id, gap, click_gap2, collect_gap, cart_gap, time])
+            elif btype == '1.2':
+                lastAccessTimeTable.append([user_id, item_id, click_gap1, gap, collect_gap, cart_gap, time])
+            elif btype == '2':
+                lastAccessTimeTable.append([user_id, item_id, click_gap1, click_gap2, gap, cart_gap, time])
+            elif btype == '3':
+                lastAccessTimeTable.append([user_id, item_id, click_gap1, click_gap2, collect_gap, gap, time])
+              
+    # 输出的文件头
+    outfile = open('last_access_time.csv', 'wb')
+    spamwriter = csv.writer(outfile, dialect = 'excel')
+    spamwriter.writerow(['user_id', 'item_id', 'click_gap1', 'click_gap2','collect_gap','cart_gap','time'])
     
     for record in lastAccessTimeTable:
         spamwriter.writerow(record)
-                
+
 def getLastAccessTime(table, key, value):
     ret = -1
     if table.get(key):
         for i in range(len(table[key])):
+#             print table[key]
             if value >= table[key][i]:
                 ret = table[key][i]
             else:
                 break
     return ret
             
+
 def addToTable(table, key, value):
     if not table.get(key):
-        table[key] = [value]
+        table[key] = [value,]
     else:
-        # 选择排序 O(n)
+        if value in table[key]:
+            return
+        # 冒泡排序 O(n)
         for i in range(len(table[key])):
             if value < table[key][i]:
                 table[key].insert(i, value)
-                
+
+
 if __name__ == '__main__':
-    lastAccessTimeOverItem()
+    initTableOverItem()
+    lastAccessTimeOverItemForBeh('1.2')   # for click2
+    lastAccessTimeOverItemForBeh('1.1')   # for click1
     
-                
+    lastAccessTimeOverItemForBeh('2')   # for collect
+    lastAccessTimeOverItemForBeh('3')   # for cart
+    
